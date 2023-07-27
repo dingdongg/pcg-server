589f7e7533ae55dd611b5faeca8e044203f5ed73
dingdongg <cs.dg.kim@gmail.com>
2023-07-26 23:03:13 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+app = Flask(__name__)
+
+@app.get("/hello")
+def hello():
+    return "hello world"
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, 
+    x_for=1, 
+    x_proto=1, 
+    x_host=1, 
+    x_prefix=1,
+)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="pcg-server",
+    version="1.0",
+    description="The backend server for my PCG game",
+    author="dingdongg",
+    author_email="cs.dg.kim@gmail.com",
+    py_modules=["app"],
+)
