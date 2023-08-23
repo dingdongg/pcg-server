937630d6bd41e86acc334c3bbac6fea7c999429b
dingdongg <cs.dg.kim@gmail.com>
2023-08-23 02:07:12 -0700

    follow litestar tutorial

--- app.py
+++ app.py
@@ -1,17 +1,40 @@
-from pydantic
-from litestar import Litestar, get
+from typing import Any
+
+from dataclasses import dataclass
+from litestar import Litestar, get, post, put
+from litestar.exceptions import NotFoundException
+
+@dataclass
+class TodoItem:
+    title: str
+    done: bool
+
+TODO_LIST: list[TodoItem] = [
+    TodoItem(title="Start writing TODO list", done=True),
+    TodoItem(title="???", done=False),
+    TodoItem(title="Profit", done=False),
+]
 
 @get("/")
-async def index() -> str:
-    return "HELLO WORLD"
+async def get_lists(done: bool = None) -> list[TodoItem]:
+    if done is None: return TODO_LIST
+    return [item for item in TODO_LIST if item.done == done]
 
-@get("/books/{book_id:int}")
-async def get_book(book_id: int) -> dict[str, int]:
-    return {
-        "book_id": book_id
-    }
+def find_item(title: str) -> TodoItem:
+    for item in TODO_LIST:
+        if item.title == title: return item
+    raise NotFoundException(detail=f"TODO {title} not found")
 
-app = Litestar([
-    index,
-    get_book,
-])
+@put("/{title:str}")
+async def update_item(title: str, data: TodoItem) -> list[TodoItem]:
+    item = find_item(title)
+    item.title = data.title
+    item.done = data.done
+    return TODO_LIST
+
+@post("/")
+async def add_item(data: TodoItem) -> list[TodoItem]:
+    TODO_LIST.append(data)
+    return TODO_LIST
+
+app = Litestar([ get_lists, add_item, update_item ])
